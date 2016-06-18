d530f60e8d7c4de7479a9a746a3f6a0ecd575df4
Timothy Sanders <tsanders@hiddenjester.com>
2016-06-18 12:02:05 -0700

    Converting from app to framework so Carthage will build.

---
+++ iOS7Colors/iOS7Colors.h
@@ -0,0 +1,19 @@
+//
+//  iOS7Colors.h
+//  iOS7Colors
+//
+//  Created by Timothy Sanders on 6/18/16.
+//  Copyright © 2016 claaslange.de. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOS7Colors.
+FOUNDATION_EXPORT double iOS7ColorsVersionNumber;
+
+//! Project version string for iOS7Colors.
+FOUNDATION_EXPORT const unsigned char iOS7ColorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOS7Colors/PublicHeader.h>
+
+
